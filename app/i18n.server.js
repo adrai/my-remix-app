3d9344c5d266e860fc917f748c309a44054d7af1
Adriano Raiano <adriano@raiano.ch>
2022-02-21 15:32:46 +0100

    locize usage

--- app/i18n.server.js
+++ app/i18n.server.js
@@ -2,6 +2,24 @@ import { RemixI18Next } from "remix-i18next";
 import { FileSystemBackend, FetchBackend } from "remix-i18next";
 import i18nextOptions from "./i18nextOptions";
 
+// import LocizeBackend from "i18next-locize-backend";
+// class LocizeBackend {
+//   constructor(options) {
+//     this.locizeBackend = new LocizeBackend(options);
+//   }
+//   async getTranslations(namespace, locale) {
+//     return new Promise((resolve, reject) => {
+//       this.locizeBackend.read(locale, namespace, (err, ret) => {
+//         if (err) return reject(err);
+//         resolve(ret);
+//       });
+//     });
+//   }
+// }
+// const backend = new LocizeBackend({
+//   projectId: "my-locize-project-id"
+// });
+
 // You will need to provide a backend to load your translations, here we use the
 // file system one and tell it where to find the translations.
 const backend = new FileSystemBackend("./public/locales");

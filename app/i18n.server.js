175e191df2bfdf6a5c8ba64fbff836c3c66d04d6
Adriano Raiano <adriano@raiano.ch>
2022-02-21 14:56:14 +0100

    reuse i18nextOptions

--- app/i18n.server.js
+++ app/i18n.server.js
@@ -1,5 +1,6 @@
 import { RemixI18Next } from "remix-i18next";
 import { FileSystemBackend, FetchBackend } from "remix-i18next";
+import i18nextOptions from "./i18nextOptions";
 
 // You will need to provide a backend to load your translations, here we use the
 // file system one and tell it where to find the translations.
@@ -11,6 +12,6 @@ const backend = new FileSystemBackend("./public/locales");
 // });
 
 export default new RemixI18Next(backend, {
-  fallbackLng: "en", // here configure your default (fallback) language
-  supportedLanguages: ["en", "de"], // here configure your supported languages
+  fallbackLng: i18nextOptions.fallbackLng, // here configure your default (fallback) language
+  supportedLanguages: i18nextOptions.supportedLngs, // here configure your supported languages
 });
